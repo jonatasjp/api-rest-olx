a2d38505fc2be2389b844dbed0444ba778ead8c8
Jonatas Luis <jonatasluis.unipe@gmail.com>
2018-03-16 20:18:09 -0300

    anuncio controller básico

--- src/main/java/com/simulando/olx/controllers/AnunciosController.java
+++ src/main/java/com/simulando/olx/controllers/AnunciosController.java
@@ -2,10 +2,16 @@ package com.simulando.olx.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.simulando.olx.custom.CustomErrorType;
 import com.simulando.olx.entidades.Anuncio;
 
 @RestController
@@ -14,13 +20,32 @@ public class AnunciosController {
 
 	@GetMapping
 	public ResponseEntity<?> buscarAnuncios(){
+		return ResponseEntity.ok(Anuncio.bancoDeAnuncios());
+	}
+	
+	@GetMapping(path="/{id}")
+	public ResponseEntity<?> buscarAnuncio(@PathVariable Integer id){
 		
-		Anuncio anuncio = new Anuncio();
-		anuncio.setCEP("58075440");
-		anuncio.setDescricao("desc");
-		anuncio.setOcultarTelefoneNoAnuncio(true);
-		anuncio.setTitulo("titulo");
+		if(id == null || id < 0 || id >= Anuncio.bancoDeAnuncios().size()) {
+			return new ResponseEntity<>(new CustomErrorType("ID inválido"), 
+					HttpStatus.NOT_FOUND);
+		}
 		
-		return new ResponseEntity<>(anuncio, HttpStatus.OK);
+		return ResponseEntity.ok(Anuncio.bancoDeAnuncios().get(id));
+	}
+
+	@PostMapping
+	public ResponseEntity<?> cadastrarAnuncio(@RequestBody Anuncio anuncio){
+		return ResponseEntity.ok("Anuncio cadastrado com sucesso!");
+	}
+
+	@DeleteMapping
+	public ResponseEntity<?> removerAnuncio(@RequestBody Anuncio anuncio){
+		return ResponseEntity.ok("Anuncio removido com sucesso!");
+	}
+	
+	@PutMapping
+	public ResponseEntity<?> atualizarAnuncio(@RequestBody Anuncio anuncio){
+		return ResponseEntity.ok("Anuncio alterado com sucesso!");
 	}
 }
---
+++ src/main/java/com/simulando/olx/custom/CustomErrorType.java
@@ -0,0 +1,12 @@
+package com.simulando.olx.custom;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public class CustomErrorType {
+
+	@Getter
+	private String message;
+
+}
